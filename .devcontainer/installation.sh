ffc5e7c32f3c335599fe767df7bc7cc08505e999
Rob Levine <rob@roblevine.co.uk>
2025-12-23 11:14:48 +0000

    chore: add claude code, uv, spec-kit

--- .devcontainer/installation.sh
+++ .devcontainer/installation.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-echo "starting installation script"
+echo "* starting installation script"
 
-echo "update and install apt packages"
+echo "** update and install apt packages"
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y vim iputils-ping dos2unix
+sudo apt install -y vim iputils-ping dos2unix telnet
 
 # Create symlink for libcrypt on ARM machines
 if [ "$(uname -m)" = "aarch64" ]; then
-    echo "Setting up libcrypt symlink for ARM64"
+    echo "** setting up libcrypt symlink for ARM64"
     sudo mkdir -p /usr/lib/aarch64-linux-gnu
     sudo ln -sf /usr/lib/aarch64-linux-gnu/libcrypt.so.1 /usr/lib/aarch64-linux-gnu/libcrypt-c6b9afc0.so.1
 fi
 
-echo "setting up Rob public key for ssh access"
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGwG5A7UQ4vQSM/SEn1ndXfgOO2RoeARNsOpnIr80du rob levine@ANOMALY" >> ~/.ssh/authorized_keys
+echo "** install uv"
+pipx install uv
 
-echo "installation script complete"
+echo "** install Claude Code"
+npm install -g @anthropic-ai/claude-code
+
+echo "** install spec-kit"
+uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
+
+echo "* installation script complete"
